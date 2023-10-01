1ab3dd76b1ca16d23ce06e72e79919d03220ee13
mohammadpiriyan <mohammadpiriyan@gmail.com>
2023-10-01 17:24:12 +0330

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="h-screen w-screen bg-blue-200 flex flex-col justify-center items-center gap-8">
+        <div>
+          <button
+            className="px-8 py-3 rounded-xl border bg-blue-100"
+            onClick={() => {
+              Notification.requestPermission().then((perm) => {
+                const notification = new Notification("Example Notification", {
+                  body: "this is more text",
+                  data: { hello: "world" },
+                  icon: "/logo512.png",
+                });
+                notification.addEventListener("error", (e) => {
+                  alert("error");
+                });
+              });
+            }}
+          >
+            push notification
+          </button>
+        </div>
+        <div className="flex justify-center items-center gap-8">
+          <button className="px-8 py-3 rounded-xl border bg-blue-100">
+            change theme
+          </button>
+          <select className="px-8 py-3 rounded-xl border bg-blue-100">
+            <option>Default</option>
+            <option>Neon</option>
+          </select>
+        </div>
+        <div>
+          <div className="theme-swiss relative bg-skin-fill max-w-4xl mx-auto overflow-hidden sm:rounded-2xl">
+            <img
+              className="absolute inset-0 h-full w-full object-cover opacity-30"
+              src="https://images.unsplash.com/photo-1613217784112-e0e197be6a0b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1600&q=80&sat=-100"
+              alt="People working on laptops"
+            />
+            <div className="absolute inset-0 bg-gradient-to-br from-skin-hue via-skin-hue to-transparent opacity-90"></div>
+            <div className="relative max-w-2xl mx-auto text-center py-16 px-4 sm:py-20 sm:px-6 lg:px-8">
+              <h2 className="text-3xl font-extrabold text-skin-base sm:text-4xl">
+                <span className="block">Focus on your content.</span>
+                <span className="block">We handle the distribution.</span>
+              </h2>
+              <p className="mt-4 text-lg leading-6 text-skin-muted">
+                Ac euismod vel sit maecenas id pellentesque eu sed consectetur.
+                Malesuada adipiscing sagittis vel nulla nec.
+              </p>
+              <div className="mt-10 max-w-sm mx-auto sm:max-w-none sm:flex sm:justify-center">
+                <div className="space-y-4 sm:space-y-0 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5">
+                  <a
+                    href="#"
+                    className="text-skin-inverted bg-skin-button-accent hover:bg-skin-button-accent-hover flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm sm:px-8"
+                  >
+                    Get started
+                  </a>
+                  <a
+                    href="#"
+                    className="text-skin-base bg-skin-button-muted flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm bg-opacity-60 hover:bg-opacity-70 sm:px-8"
+                  >
+                    Live demo
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
